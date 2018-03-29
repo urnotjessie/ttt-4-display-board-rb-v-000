5e603db85798abc44a94937697df554dd9972149
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-29 14:37:53 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -187,11 +187,11 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
-      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   |   |   ")
+      expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq("   |   |   ")
+      expect(rows[4]).to eq(" O | O | O ")
 
       # *** Comment the line below by adding a # at the line start ***
       # expect(true).to be(true)
