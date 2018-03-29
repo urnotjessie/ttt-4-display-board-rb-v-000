012f3ac90a5ff2b09ad39c14a6d45e477b963d43
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-29 14:36:35 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -182,11 +182,16 @@ describe "#display_board in 'lib/display_board.rb" do
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
-      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+      board = ["O", "O", "O", "O", "I", "O", "O", "O", "O"]
 
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
 
+      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("   |   |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("   |   |   ")
 
       # *** Comment the line below by adding a # at the line start ***
       # expect(true).to be(true)
